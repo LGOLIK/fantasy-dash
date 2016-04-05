ef9f58fa534433e345911f6577da3853d4f0589e
LGOLIK <golikster@gmail.com>
2016-04-05 15:59:06 -0400

    brought back what was working this morning, cleaned up some of the code

--- public/js/script.js
+++ public/js/script.js
@@ -72,71 +72,99 @@ function showSingleTeamGraph(data) {
   // set the height and width of the graph
   let width = 960;
   let height = 500;
-  let padding = 40;
 
-  // here are the scale definitions
+  // set the colors of the circles with a predefined d3 color scale
+  let colors = d3.scale.category10();
+
+  // set the scales
 
   // x scale
-  let xScale = d3.scale.linear()
+  let x = d3.scale.linear()
     .domain(d3.extent(data, function (d) {
-      return d.round;
-    }))
-    .range([padding, width - margins.left - margins.right]);
+    return d.round;
+  }))
+    // the range maps the domain to values from 0 to the width minus the left and right margins (used to space out the visualization)
+    .range([0, width - margins.left - margins.right]);
 
-  // y scale
-
-  // first get a unique list of the positions
+  // get a unique list of player positions for the y axis
   let positions = _.uniq(_.map(data, function(value) {
     return value.position
-  }));
+  }))
 
-  let yScale = d3.scale.ordinal()
+  // y scale
+  let y = d3.scale.ordinal()
     .domain(positions)
+
+    // Note that height goes first due to the weird SVG coordinate system
     .rangePoints([height - margins.top - margins.bottom, 0]);
 
-
-  // here are the axis definitions
-
-  // x axis
+  // this is the actual definition of our x and y axes. The orientation refers to where the labels appear - for the x axis, below or above the line, and for the y axis, left or right of the line. Tick padding refers to how much space between the tick and the label. There are other parameters too - see https://github.com/mbostock/d3/wiki/SVG-Axes for more information
   let xAxis = d3.svg.axis()
-    .scale(xScale)
+    .scale(x)
     .orient('bottom')
     .ticks(20);
-
-  // y axis
   let yAxis = d3.svg.axis()
-    .scale(yScale)
+    .scale(y)
     .orient('left')
-    .tickPadding(2);
+    .ticks(10);
 
-  // create svg element and append it to the dashboard div
+  // add this svg component to the dashboard div
   let svg = d3.select('#dashboard')
     .append('svg')
     .attr('width', width)
-    .attr('height', height);
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + margins.left + ',' + margins.top + ')');
 
-  // create the x axis
+  // we add the axes to the SVG component. At this point, this is just a placeholder. The actual axis will be added in a bit
+  // x axis
   svg.append('g')
     .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + (height - padding + 5) + ')')
-    .call(xAxis);
+    .attr('transform', 'translate(0,' + y.range()[0] + ')');
+  // y axis
+  svg.append('g').attr('class', 'y axis');
 
-  // create the y axis
-  svg.append('g')
-    .attr('class', 'y axis')
-    .attr('transform', 'translate(0,' + (padding + 5) + ')')
-    .call(yAxis);
+  // this is our X axis label. formatting can be added here
+  svg.append('text')
+    .attr('fill', '#414241')
+    .attr('text-anchor', 'end')
+    .attr('x', width / 2)
+    .attr('y', height - 15)
+    .text('Draft Rounds');
 
-  // select the x axis in the chart
-  svg.selectAll('x.axis')
-    .call(xAxis);
+  // this is where we select the axis we created a few lines earlier. See how we select the axis item. in our svg we appended a g element with a x/y and axis class. To pull that back up, we do this svg select, then 'call' the appropriate axis object for rendering.
+  svg.selectAll('g.y.axis').call(yAxis);
+  svg.selectAll('g.x.axis').call(xAxis);
 
-  // select the y axis in the chart
-  svg.selectAll('y.axis')
-    .call(yAxis);
+  // now, we can get down to the data part, and drawing stuff. We are telling D3 that all nodes (g elements with class node) will have data attached to them. The 'key' we use (to let D3 know the uniqueness of items) will be the name. Not usually a great key, but fine for this example.
+  let player = svg.selectAll('g.node').data(data, function (d) {
+    return d.player_name;
+  });
 
+  // we 'enter' the data, making the SVG group (to contain a circle and text) with a class node. This corresponds with what we told the data it should be above.
+  let playerGroup = player.enter().append('g').attr('class', 'node')
 
+  // this is how we set the position of the items. Translate is an incredibly useful function for rotating and positioning items
+  .attr('transform', function (d) {
+    return 'translate(' + x(d.round) + ',' + y(d.position) + ')';
+  });
 
+  // we add our first graphics element! A circle!
+  playerGroup.append('circle')
+    .attr('r', 5)
+    .attr('class', 'dot')
+    .style('fill', function (d) {
+      // Now each node will be coloured
+      // by the player's position
+      return colors(d.position);
+   });
 
-
+  // now we add some text, so we can see what each item is.
+  playerGroup.append('text')
+    .style('text-anchor', 'middle')
+    .attr('dy', -10)
+    .text(function (d) {
+      // this shouldn't be a surprising statement.
+      return d.player_name;
+   });
 }
