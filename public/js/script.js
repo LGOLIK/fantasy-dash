8c2e8fc61975326060e3deeabb8e189c87dca0d3
LGOLIK <golikster@gmail.com>
2016-04-04 20:11:31 -0400

    ajax get to single team draft results connects as expected

--- public/js/script.js
+++ public/js/script.js
@@ -42,68 +42,30 @@ function renderTeams(data, $ul) {
 }
 
 function clickTeam() {
-  $('.team-list').on('click', () => {
+  $('.team-list').on('click', (event) => {
     event.stopProbagation;
-    console.log('clicked!');
+
+    let $teamID = $(event.target).attr('id');
 
     let $dashboard = $('#dashboard');
     $dashboard.empty();
 
-    let $teamID;
+    let teamDraftData = [];
+    // get the draft results route for a single team
+    $.get(`/apis/draftresults/${$teamID}`)
+    .done( (data) => {
+      renderTeamDraftResult(teamDraftData, data);
+      console.log(teamDraftData);
+    })
+    .fail( () => {
+      console.error('You failed. Go fix it.');
+    });
 
-  //   var data = [[5,3], [10,17], [15,4], [2,8]];
-  //
-  //   var margin = {top: 20, right: 15, bottom: 60, left: 60}
-  //     , width = 960 - margin.left - margin.right
-  //     , height = 500 - margin.top - margin.bottom;
-  //
-  //   var x = d3.scale.linear()
-  //             .domain([0, d3.max(data, function(d) { return d[0]; })])
-  //             .range([ 0, width ]);
-  //
-  //   var y = d3.scale.linear()
-  //   	      .domain([0, d3.max(data, function(d) { return d[1]; })])
-  //   	      .range([ height, 0 ]);
-  //
-  //   var chart = d3.select('#dashboard')
-	// .append('svg:svg')
-	// .attr('width', width + margin.right + margin.left)
-	// .attr('height', height + margin.top + margin.bottom)
-	// .attr('class', 'chart')
-  //
-  //   var main = chart.append('g')
-	// .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-	// .attr('width', width)
-	// .attr('height', height)
-	// .attr('class', 'main')
-  //
-  //   // draw the x axis
-  //   var xAxis = d3.svg.axis()
-	// .scale(x)
-	// .orient('bottom');
-  //
-  //   main.append('g')
-	// .attr('transform', 'translate(0,' + height + ')')
-	// .attr('class', 'main axis date')
-	// .call(xAxis);
-  //
-  //   // draw the y axis
-  //   var yAxis = d3.svg.axis()
-	// .scale(y)
-	// .orient('left');
-  //
-  //   main.append('g')
-	// .attr('transform', 'translate(0,0)')
-	// .attr('class', 'main axis date')
-	// .call(yAxis);
-  //
-  //   var g = main.append("svg:g");
-  //
-  //   g.selectAll("scatter-dots")
-  //     .data(data)
-  //     .enter().append("svg:circle")
-  //         .attr("cx", function (d,i) { return x(d[0]); } )
-  //         .attr("cy", function (d) { return y(d[1]); } )
-  //         .attr("r", 8);
-  // });
+  });
+
+}
+
+// function to render the draft result data and get it into a view for d3
+function renderTeamDraftResult(teamDraftData, data) {
+  teamDraftData.push(data)
 }
